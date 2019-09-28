3263c9819234776024dd2c1addd84caf510d96ec
Urvee Trivedi <urvee.trivedi@rutgers.edu>
2019-09-27 22:55:23 -0400

    completed project

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+
+
+    //prefab for instantiating apples
+
+
+
+    public GameObject applePrefab;
+    //speed at which tree moves
+    public float speed = 1f;
+
+    //distance tree turns
+
+    public float leftAndRightEdge = 10f;
+
+    //chance it will change directions
+
+    public float chanceToChangeDirections = 0.1f;
+
+    //rate at which apples will be inisiated
+
+    public float secondsBetweenAppleDrops = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Invoke("DropApple", 2f);
+    }
+
+
+
+
+    void DropApple()
+    {
+
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // basic movement
+
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+
+        // changing direction
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);  //move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // move left
+
+        }
+        else if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+
+
+        void FixedUpdate()
+        {
+            if (Random.value < chanceToChangeDirections)
+            {
+                speed *= -1;
+            }
+        }
+
+
+    }
+}
---
+++ Assets/HighScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Awake()
+    {
+        //if playerPrefs highScore
+
+        if (PlayerPrefs.HasKey("High Score"))
+        {
+            score = PlayerPrefs.GetInt("High Score");
+        }
+
+
+        //assign the high score to high score
+
+        PlayerPrefs.SetInt("High Score", score);
+
+    }
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        //update the playerprefs highscore
+
+        if (score > PlayerPrefs.GetInt("High Score"))
+        {
+
+            PlayerPrefs.SetInt("High Score", score);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+}
